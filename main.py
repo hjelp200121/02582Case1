930e86ebad64c959a8113f0f74f88ec660a06946
Bastian Ravn <Bastian>
2025-03-15 12:44:32 +0100

    one hot encoding categorical data and standardizing continuous data

--- main.py
+++ main.py
@@ -2,26 +2,39 @@ import pandas as pd
 import numpy as np
 from sklearn import linear_model
 from sklearn.model_selection import GridSearchCV
-
+from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
 
 data_path = "data/cleaned_data_small.csv"
 df = pd.read_csv(data_path)
 
 y = np.array(df[df.columns[0]])
-X = np.array(df[df.columns[1:101]])
-
-# ved ikke om vi måske skal normalize dataen før vi træner med den?
+X = np.array(df[df.columns[1:]])
 
 N, p = X.shape
 
+# train/test split
 X_train = X[0:70, :]
 y_train = y[0:70]
-
 X_test = X[70:, :]
 y_test = y[70:]
 
+# split train/test splits into continuous and categorical
+X_train_cont = X_train[:, 0:95].copy()
+X_train_cate = X_train[:, 95:].copy()
+X_test_cont = X_test[:, 0:95].copy()
+X_test_cate = X_test[:, 95:].copy()
 
-model = linear_model.ElasticNet(alpha=1)
+# standardize only continous data
+X_train_cont = preprocessing.scale(X_train_cont)
+X_test_cont = preprocessing.scale(X_test_cont)
+
+# reassemble train/test split
+X_train = np.concatenate((X_train_cont, X_train_cate), axis=1)
+X_test = np.concatenate((X_test_cont, X_test_cate), axis=1)
+
+
+model = linear_model.ElasticNet(alpha=0.1)
 
 model.fit(X_train, y_train)
 
